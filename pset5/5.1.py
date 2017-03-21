86fc092478c8d676aab9f7d9a98c8cf449256366
perimyotis <tuf44490@temple.edu>
2017-03-21 15:59:42 -0400

    initial commit

---
+++ pset5/5.1.py
@@ -0,0 +1,17 @@
+import pysam 
+
+open ("Intersect_filtered_cov8_chr21_rand1000.bed") as f:
+	for line in f:
+		L =line.strip().split()
+
+		for given site:
+		get allele freq
+
+			if freq == 0:
+				not seq
+			elif freq == n:
+				not seq
+			else: 
+				seg =+1
+	
+	
